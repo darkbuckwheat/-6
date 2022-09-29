7deff4068a4c7832a4c89ecda692c36e5218b0c7
darkbuckwheat <labyntsev.g@mail.ru>
2022-09-29 21:21:26 +0300

    Initial commit

---
+++ src/Client/utillity/Color.java
@@ -0,0 +1,20 @@
+package Client.utillity;
+
+public enum Color {
+    RED("\u001B[31m"),
+    GREEN("\u001B[32m");
+
+    private final String code;
+
+    Color(String code) {
+        this.code = code;
+    }
+
+    public String toString() {
+        return code;
+    }
+
+    public String colorize(String message) {
+        return this.toString() + message + "\u001B[0m";
+    }
+}
